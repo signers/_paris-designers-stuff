e7abd75e169557cc933ada1b113827eb327ec0f5
Laurent Leveau <ui.apps@icloud.com>
2014-05-01 00:01:04 +0200

    updated
    updated

--- config.rb
+++ config.rb
@@ -4,7 +4,7 @@ require 'compass/import-once/activate'
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
-sass_dir = "sass"
+sass_dir = "css/sass"
 images_dir = "images"
 javascripts_dir = "scripts"
 fonts_dir = "fonts"
@@ -12,7 +12,8 @@ fonts_dir = "fonts"
 output_style = :compressed
 environment = :development
 
-relative_assets = true
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
@@ -23,4 +24,4 @@ color_output = false
 # project again passing --syntax sass, or you can uncomment this:
 # preferred_syntax = :sass
 # and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+# sass-convert -R --from scss --to sass css/sass scss && rm -rf sass && mv scss sass
